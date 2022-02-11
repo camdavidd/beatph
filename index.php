58cda172102189ab5428e9578fd9e51ee5767ec2
unknown <camdavidbaladjay@gmail.com>
2022-02-11 21:43:08 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,267 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="BEAT PH is the newest mobile Bus Application where in just one tap, you can now find a nearby bus, share your location with your family and friends, access the live feed and view the traffic ahead, with free WiFi on board.">
+        <link rel="icon" href="img/beatfav.png" type="image/png">
+        <title>BEAT PH</title>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="vendors/linericon/style.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+        <link rel="stylesheet" href="vendors/lightbox/simpleLightbox.css">
+        <link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
+        <link rel="stylesheet" href="vendors/animate-css/animate.css">
+        <link rel="stylesheet" href="vendors/popup/magnific-popup.css">
+        <!-- main css -->
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/responsive.css">
+        <link rel="stylesheet" href="css/animate.css"/>
+          <!-- Animation-->
+          <script src="js/wow.min.js"></script>
+    </head>
+    <body data-spy="scroll" data-target="#mainNav" data-offset="70">
+        
+        <!--================Header Menu Area =================-->
+        <?php include ('header-nav.php')?>
+        <!--================Header Menu Area =================-->
+        
+        <!--================Home Banner Area =================-->
+        <section class="home_banner_area" id="home">
+            <div class="banner_inner">
+				<div class="container">
+					<div class="row banner_content">
+						<div class="col-lg-9">
+							<h2>BEAT PH</h2>
+							<p>is the newest mobile Bus Application, riding a bus is about to get easier! Coming soon on the Play Store!</p> 
+							<a class="banner_btn" href="#overview">Explore Now</a>
+						</div>
+						<div class="col-lg-3">
+							<div class="banner_map_img wow animated tada">
+								<img class="img-fluid" src="img/mobile.png" alt="">
+							</div>
+						</div>
+					</div>
+				</div>
+            </div>
+        </section>
+        <!--================End Home Banner Area =================-->
+
+        <!--================Feature Area =================-->
+        <section class="feature_area p_120" >
+        	<div class="container">
+        		<div class="interior_inner row"id="overview">
+                    <div class="col-lg-8 offset-lg-2 wow animated fadeInUp">
+                       <div style="width:100%;height:0px;position:relative;padding-bottom:66.667%;"><iframe src="https://streamable.com/s/krtk4/gyfdis" frameborder="0" width="100%" height="100%" allowfullscreen style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden;"></iframe></div>
+                    </div>
+                    <div class="col-lg-8 offset-lg-2">
+                        <div class="interior_text">
+                            <br><br> 
+                            <p>Commuting has become one of Metro Manila residents’ major burdens for decades now and fixing this problem has become one of the most difficult puzzles for the government to solve.
+                            </br></br>Needless to say, are the hours spent on the road which affects workplace productivity, business costs, health and even human relationships in all forms.</br></br>It is then with these factors that KonganBuddies Marketing Inc., has ventured into developing a new mobile application that will ease the sacrifices of our commuting public through an application that will give them the power to control their journeys on the road and not the other way around.
+                            </p>
+                           <!-- <a class="main_btn" href="#">See Details</a>-->
+                        </div>
+                    </div>
+ 
+                    </div>
+
+                    
+                </div>
+
+        	</div>
+        </section>
+        <!--================End Feature Area =================-->
+       <!--================Interior Area =================-->
+        <section class="interior_area " id="about">
+            <div class="container">
+                <div class="interior_inner row">
+                    <div class="col-lg-5 offset-lg-1">
+                        <div class="interior_text">
+                            <h4>About The App</h4>
+                            <p><b style="color:black">BEAT PH</b> is the newest mobile Bus Application where in just one tap, you can now find a nearby bus, share your location with your family and friends, access the live feed and view the traffic ahead, with free WiFi on board! 
+</p>
+                            <a class="main_btn" href="#features">Learn More</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="hero wow animated bounceInLeft">
+                            <div class="highway"></div>
+                            <div class="cityback"></div>
+                            <div class="car">
+                             <img src="img/bus.png">
+                            </div>
+                            <div class="wheel">
+                                <img src="img/kuyawheel.png" class="back-wheel">
+                                <img src="img/kuyawheel.png" class="front-wheel">
+                            </div>
+                        <!--<img class="img-fluid" src="img/bus.png" alt=""> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--================End Interior Area =================-->
+         <section class="feature_area p_120" >
+            <div class="container">
+                <div class="interior_inner row">
+                    <div class="col-lg-5 offset-lg-1">
+                        <div class="interior_text">
+                            <h4>Our Mission</h4>
+                            <p>To provide the best security and convenience to the Filipino bus passengers.  </p>
+                            
+                        </div>
+                    </div>
+                    <div class="col-lg-5 offset-lg-1">
+                        <div class="interior_text">
+                            <h4>Our Vision</h4>
+                            <p>To become the best choice of every bus company in the Philippines by providing secured and convenient travel for the commuters.</p>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--================End Feature Area =================-->
+        <!--================Feature Area =================-->
+        <section class="banner_area2 p_120" id="features">
+
+            <div class="container">
+                <div class="main_title">
+                    <h2 style="color:#ffff">Features</h2>
+                </div>
+                <div class="feature_inner row">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="l_news_item">
+                        <center> <img src="img/icon/cam.png" alt=""> </center>
+                            <h4>Live feed</h4>
+                            <p>The live feed feature is for the security purposes and letting you access the dash camera view.<br><br></p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="l_news_item">
+                           <center> <img src="img/icon/wifi.png" alt=""></center>
+                            <h4>WIFI</h4>
+                            <p>You can now surf the net while on board FOR FREE!<br><br><br><br></p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="l_news_item">
+                           <center> <img src="img/icon/people.png" alt=""></center>
+                            <h4>People Count</h4>
+                            <p>You can now also recognize if the bus is overcrowded or there's an available seat waiting for you.<br><br></p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="l_news_item">
+                            <center><img src="img/icon/ticket.png" alt=""></center>
+                            <h4>Ticketing</h4>
+                            <p>At last! You don’t have to line up and waste your time waiting to buy your tickets because you can now book your tickets online!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--================End Feature Area =================-->
+       <!--================Contact Area =================-->
+        <section class="contact_area p_120" id="contactus">
+            <div class="container">
+                
+                <div class="row">
+                    <div class="col-lg-3">
+                        <div class="contact_info">
+                            <div class="info_item">
+                                <i class="lnr lnr-home"></i>
+                                <h6>PBCOM Tower</h6>
+                                <p>
+                                6795 Ayala Avenue, Corner V.A. Rufino St, Makati, 1226
+                                </p>
+                            </div>
+                            <div class="info_item">
+                                <i class="lnr lnr-phone-handset"></i>
+                                <h6><a href="#">0917-129-3304 / (02)-7-987-4644
+                                </a></h6>
+                            </div>
+                            <br>
+                            <div class="info_item">
+                                <i class="lnr lnr-envelope"></i>
+                                <h6><a href="#">info@beat.ph
+                                </a></h6>
+                                <p>Send us your query anytime!</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-9">
+                        <form class="row contact_form" action="#" method="post" id="contactForm" novalidate="novalidate">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" class="form-control" id="email" name="email" placeholder="Enter email address">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control" id="subject" name="subject" placeholder="Enter Subject">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <textarea class="form-control" name="message" id="message" rows="1" placeholder="Enter Message"></textarea>
+                                </div>
+                            </div>
+                            <div class="col-md-12 text-right">
+                                <button type="submit" value="submit" class="btn main_btn">Send Message</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--================Contact Area =================-->
+        <!--================ start footer Area  =================-->	
+        <footer class="footer-area p_100">
+            <div class="container">
+
+                <div class="row footer-bottom d-flex justify-content-between align-items-center">
+                    <p class="col-lg-8 col-md-8 footer-text m-0">Copyright © 2020 BEAT PH</p>
+                    <div class="col-lg-4 col-md-4 footer-social">
+                        <a href="#"><i class="fa fa-facebook "></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-dribbble"></i></a>
+                        <a href="#"><i class="fa fa-behance"></i></a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+		<!--================ End footer Area  =================-->
+    <!-- ON SCROLL ANIMATION -->
+    <script>
+      new WOW().init();
+    </script>
+        
+        
+        
+        
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="js/jquery-3.2.1.min.js"></script>
+        <script src="js/popper.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/stellar.js"></script>
+        <script src="vendors/lightbox/simpleLightbox.min.js"></script>
+        <script src="vendors/nice-select/js/jquery.nice-select.min.js"></script>
+        <script src="vendors/isotope/imagesloaded.pkgd.min.js"></script>
+        <script src="vendors/isotope/isotope-min.js"></script>
+        <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+        <script src="js/jquery.ajaxchimp.min.js"></script>
+        <script src="vendors/counter-up/jquery.waypoints.min.js"></script>
+        <script src="vendors/counter-up/jquery.counterup.js"></script>
+        <script src="js/mail-script.js"></script>
+        <script src="vendors/popup/jquery.magnific-popup.min.js"></script>
+        <script src="js/theme.js"></script>
+
+    </body>
+</html>
